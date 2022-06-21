02c4602a5f36e731e37001a7f9f8528eaae815c2
Apekshahande <apeksha18@navgurukul.org>
2022-06-21 08:21:18 +0530

    .

--- common/Layout.js
+++ common/Layout.js
@@ -12,7 +12,7 @@ const Layout =({children})=>{
             <title> PlayCub</title>
             <meta name="description" content="Interactive Coding Classes for Kids. Teach your kids how to build websites with fun. Private 1-1 classes"></meta>
             <link  rel="shortcut icon" href="/Playcub-1.ico" type="image/x-icon"/>
-            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet"></link>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet"></link>
         </Head>
         <ThemeProvider theme={theme}>
             <Navbar/>
