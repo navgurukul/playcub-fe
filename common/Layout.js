c3327bd3cc1409c359d46c76de186c615f4e20ed
Apekshahande <apeksha18@navgurukul.org>
2022-07-02 16:10:17 +0530

    Added icons

--- common/Layout.js
+++ common/Layout.js
@@ -26,9 +26,9 @@ const Layout =({children})=>{
             <meta name="description" content="Interactive Coding Classes for Kids. Teach your kids how to build websites with fun. Private 1-1 classes"></meta>
             <link  rel="apple-touch-icon" sizes="180*180" href="../apple-touch-icon.png"/>
 
-            {/* <link  rel="icon"sizes="32*32" type="image/png" href="../favicon-32*32.png" />
-            <link  rel="icon" sizes="16*16" type="image/png" href="../favicon-16*16.png" />
-            <link  rel="manifest" href="../site.webmanifest" /> */}
+            <link  rel="icon"sizes="32*32" type="image/png" href="../favicon-32x32.png" />
+            <link  rel="icon" sizes="16*16" type="image/png" href="../favicon-16x16.png" />
+            <link  rel="manifest" href="../site.webmanifest" />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap" rel="stylesheet"></link>
         </Head>
         <ThemeProvider theme={theme} >
